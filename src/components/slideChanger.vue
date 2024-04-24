493cf1908010bc18d4f87f912dffa7f4d6aa4610
abderrahim40k4 <abderrahim.ait.oufassi2004@gmail.com>
2024-04-24 18:45:23 +0100

    slide changer view

---
+++ src/components/slideChanger.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full flex flex-row justify-end items-center">
+        <img src="../assets/icons/right.svg">
+        <div class="h-[24px] flex flex-row justify-center items-center">
+            <div class="h-[13px] w-[13px] hover:h-[16px] hover:w-[16px] rounded-full  bg-[#b3b3b3] text-white-color/20  hover:bg-dark-blue hover:text-white-color font-thin text-[10px] hover:text-[12px] justify-center mx-1 flex items-center">1</div>
+            <div class="h-[13px] w-[13px] hover:h-[16px] hover:w-[16px] rounded-full  bg-[#b3b3b3] text-white-color/20  hover:bg-dark-blue hover:text-white-color font-thin text-[10px] hover:text-[12px] justify-center mx-1 flex items-center">1</div>
+            <div class="h-[13px] w-[13px] hover:h-[16px] hover:w-[16px] rounded-full  bg-[#b3b3b3] text-white-color/20  hover:bg-dark-blue hover:text-white-color font-thin text-[10px] hover:text-[12px] justify-center mx-1 flex items-center">1</div>
+            
+        </div>
+        <img src="../assets/icons/left.svg">
+    </div>
+</template>
+<script setup>
+
+</script>
