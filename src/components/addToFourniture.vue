4f3835af76546fca35f669da5c748394717e14c5
abderrahim40k4 <abderrahim.ait.oufassi2004@gmail.com>
2024-04-29 19:19:39 +0100

    Update structure

--- src/components/addToFourniture.vue
+++ src/components/addToFourniture.vue
@@ -4,20 +4,31 @@
             <img src="../assets/icons/back.svg">
         </div>
       <div class="w-full h-full pb-10">
-        <div class="flex flex-row justify-between items-center mt-8">
-        <div class="w-1/4">
+        <div class="grid grid-cols-3 gap-x-20 gap-y-8 justify-between items-center mt-8">
+        <div class="">
             <label class="font-Poppins font-medium text-sm text-[#6D6D6D]">Sélectionner la ville</label>
             <input class="w-full p-3 rounded-full border border-sky-color mt-4 text-sm font-Poppins font-medium" placeholder="Casablanca">
         </div>
-        <div class="w-1/4">
+        <div class="">
             <label class="font-Poppins font-medium text-sm text-[#6D6D6D]">Sélectionner l’école</label>
             <input class="w-full p-3 rounded-full border border-sky-color mt-4 text-sm font-Poppins font-medium" placeholder="URC">
         </div>
-        <div class="w-1/4">
+        <div class="">
             <label class="font-Poppins font-medium text-sm text-[#6D6D6D]">Sélectionner la classe</label>
             <input class="w-full p-3 rounded-full border border-sky-color mt-4 text-sm font-Poppins font-medium" placeholder="3eme">
         </div>
+        <div class="">
+            <label class="font-Poppins font-medium text-sm text-[#6D6D6D]">Sélectionner les languges</label>
+            <input class="w-full p-3 rounded-full border border-sky-color mt-4 text-sm font-Poppins font-medium" placeholder="3eme">
         </div>
+        <div class="">
+            <label class="font-Poppins font-medium text-sm text-[#6D6D6D]">Ajouter la liste format PDF</label>
+            <input class="w-full p-3 rounded-full border border-sky-color mt-4 text-sm font-Poppins font-medium" placeholder="3eme">
+        </div>
+        </div>
+
+
+        
       </div>
     </div>
 
