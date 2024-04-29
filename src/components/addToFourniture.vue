3983ad0f59be5ace6f672360edce6c453c8e7f46
abderrahim40k4 <abderrahim.ait.oufassi2004@gmail.com>
2024-04-29 19:10:17 +0100

    class  ville ecole

--- src/components/addToFourniture.vue
+++ src/components/addToFourniture.vue
@@ -1,12 +1,23 @@
 <template class="w-4/5">
-    <div class="h-full p-16 w-4/5 flex justify-center items-center">
+    <div class="h-full p-16 w-4/5">
+        <div>
+            <img src="../assets/icons/back.svg">
+        </div>
       <div class="w-full h-full pb-10">
-        <select name="cars" id="cars" multiple>
-    <option value="volvo">Volvo</option>
-    <option value="saab">Saab</option>
-    <option value="opel">Opel</option>
-    <option value="audi">Audi</option>
-  </select>
+        <div class="flex flex-row justify-between items-center mt-8">
+        <div class="w-1/4">
+            <label class="font-Poppins font-medium text-sm text-[#6D6D6D]">Sélectionner la ville</label>
+            <input class="w-full p-3 rounded-full border border-sky-color mt-4 text-sm font-Poppins font-medium" placeholder="Casablanca">
+        </div>
+        <div class="w-1/4">
+            <label class="font-Poppins font-medium text-sm text-[#6D6D6D]">Sélectionner l’école</label>
+            <input class="w-full p-3 rounded-full border border-sky-color mt-4 text-sm font-Poppins font-medium" placeholder="URC">
+        </div>
+        <div class="w-1/4">
+            <label class="font-Poppins font-medium text-sm text-[#6D6D6D]">Sélectionner la classe</label>
+            <input class="w-full p-3 rounded-full border border-sky-color mt-4 text-sm font-Poppins font-medium" placeholder="3eme">
+        </div>
+        </div>
       </div>
     </div>
 
