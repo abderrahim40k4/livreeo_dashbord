ee5b3cfaa8e150f4d3e5e9293b2fde43c47788bf
abderrahim40k4 <abderrahim.ait.oufassi2004@gmail.com>
2024-05-13 16:24:26 +0100

    creat  store

---
+++ src/stors/defultStore.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+
+export const useDefulteStore = defineStore('defulteStore', {
+  
+    state: () => ({
+        
+    }),
+    getters: {
+
+    },
+    actions:{
+        
+    }
+})
