89ba272060cf3e02a7ab35fcf4ede0a4ab3219d9
abderrahim40k4 <abderrahim.ait.oufassi2004@gmail.com>
2024-05-13 12:59:54 +0100

    update

--- src/components/dailog/addBook.vue
+++ src/components/dailog/addBook.vue
@@ -27,15 +27,18 @@
             leave-to="opacity-0 scale-95"
           >
             <DialogPanel
-              class="w-3/5 ml-64 transform overflow-hidden bg-white-color p-8 text-left align-middle shadow-xl transition-all rounded-xl"
+              class="w-3/5 ml-64 transform overflow-hidden bg-white-color p-8 text-left align-middle shadow-xl transition-all"
             >
               <div class="mt-4 w-full flex flex-row justify-evenly p-8">
                 
-                  <div class="w-2/5 border border-dark-blue p-4 " :class="active ? 'rounded-lg' :'rounded-full'">
-                    <Listbox v-model="selectedPerson" as="div" @click="handdelling" >
-                    <ListboxButton>{{ selectedPerson.name }}</ListboxButton>
-                    <ListboxOptions>
-                      <ListboxOption
+                  <div class="w-2/5" :class="active ? 'rounded-lg' :'rounded-full'">
+                    <div class="my-4 text-dark-blue font-Poppins font-normal text-sm">sélectionner un livre qui a déjà été traité.</div>
+                    <Listbox v-model="selectedPerson" as="div"  class="border-2 border-dark-blue p-4 " :class="active ? 'rounded-2xl' :'rounded-full'" @click="handdelling" >
+                      
+                    <ListboxButton class="font-Poppins font-normal text-[16px] text-[#B0B7BC] w-full inline-flex items-start justify-start">{{ selectedPerson.name }}</ListboxButton>
+                    <ListboxOptions class="border-t-2 border-dark-blue">
+                      <ListboxOption as="div"
+                      class="font-Poppins font-normal text-[16px] text-dark-blue py-2 w-full inline-flex items-start justify-start"
                         v-for="person in people"
                         :key="person.id"
                         :value="person"
@@ -48,6 +51,7 @@
                   </div>
 
                   <div class="w-2/5">
+                    <div class="my-4 text-dark-blue font-Poppins font-normal text-sm">sélectionner un livre qui a déjà été traité.</div>
                     <button class="w-full inline-flex justify-center items-center p-3 bg-dark-blue rounded-full" @click="$emit('newbook')">
                       <img src="../../assets/icons/add-new.svg" alt="">
                     </button>
